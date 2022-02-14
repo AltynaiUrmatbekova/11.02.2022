915e0f8441a6410831abbe9a2a5ccfa56ba245e1
AltynaiUrmatbekova <64131999+AltynaiUrmatbekova@users.noreply.github.com>
2022-02-14 13:25:26 +0600

    input type text

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.body.addEventListener('dblclick', function(event) {
 });
 
 let example = document.createElement('section');
-example.style.width = '20px';
+example.InnerHTML = 'E';
 example.style.height = '20px'
 example.style.background = 'black';
 example.style.position = 'absolute';
@@ -29,3 +29,29 @@ example.style.left = event.clientX +'px'
 
 });
 
+document.body.addEventListener('mousedown', function() {
+    console.log('mousedown');
+})
+document.body.addEventListener('mouseup', function() {
+    console.log('mouseup');
+})
+
+let example2 = document.querySelector('#example2');
+
+example2.addEventListener('keydown', function(event) {
+    console.log('keydown', event);
+    const preventList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    if (preventList.includes(event.key)) {
+        event.preventDefault();
+    }
+    
+});
+example2.addEventListener('keyup', function(event) {
+    console.log('keyup', event);
+});
+
+let example3 = document.querySelector('#example3');
+let example3span = document.querySelector('#example3span');
+example3.addEventListener('keydown', function(event) {
+    example3span.textContent = example3.value
+});
